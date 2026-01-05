fae366bc085eefe6e7e576d3d8ed224aac433675
Shahriar-em0n <contactshahriar12@gmail.com>
2026-01-05 15:44:48 +0600

    Add the probelm

---
+++ Loop-problem/006.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Emter the number";
+    cin >> n;
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << i * n << endl;
+    }
+
+    return 0;
+}
