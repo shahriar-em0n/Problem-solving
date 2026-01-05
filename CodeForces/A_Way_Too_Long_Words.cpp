a1131609aabbe21be8b50c97ba64e0178e1c0526
Shahriar-em0n <contactshahriar12@gmail.com>
2026-01-05 15:42:22 +0600

    Add the codeforces folder and one problem

---
+++ CodeForces/A_Way_Too_Long_Words.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    string w;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> w;
+        if (w.length() > 10)
+        {
+            cout << w[0] << w.length() - 2 << w[w.length() - 1] << endl;
+        }
+        else
+        {
+            cout << w << endl;
+        }
+    }
+
+    return 0;
+}
